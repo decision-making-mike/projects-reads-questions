3140838fc75fbe6d034f9843a13b31339f224c34
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2025-10-14 21:40:20 +0200

    Sort showcase project comment improvement

--- projects/data-science/sort-showcase/12-improved/sort.bash
+++ projects/data-science/sort-showcase/12-improved/sort.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is an improved version of solution 12. It revealed to me when I was reading the GNU sort's manual (https://www.gnu.org/software/coreutils/manual/html_node/sort-invocation.html). I discovered I used the idiom the manual mentions, but with an additional, bug-prone swapping (of which an additional hindrance, beside being bug-prone, might be the time cost, but I'm not sure, as on my machine it runs quite fast for 100k data). So, compared to the not improved version, this one avoids the swapping, and additionally replaces the second longer GAWK script with shorter cut.
+# This is an improved version of solution 12. It revealed to me when I was reading the GNU sort's manual (https://www.gnu.org/software/coreutils/manual/html_node/sort-invocation.html). I discovered I used the decorate-sort-undecorate idiom which the manual mentions (see https://en.wikipedia.org/wiki/Schwartzian_transform), but with an additional, bug-prone swapping (of which an additional hindrance, beside being bug-prone, might be the time cost, but I'm not sure, as on my machine it runs quite fast for 100k data). So, compared to the not improved version, this one avoids the swapping, and additionally replaces the second longer GAWK script with shorter cut invokation.
 
 gawk '
     BEGIN {
